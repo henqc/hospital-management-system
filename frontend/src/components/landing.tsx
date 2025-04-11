344efb28bab27814fb2d21161c83f5be2f4619ae
Henry Chen <henryq.chen101@gmail.com>
2025-04-11 00:46:28 -0400

    Login bare bones and test nav

--- frontend/src/components/landing.tsx
+++ frontend/src/components/landing.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export function Landing() {
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <div>Home</div>
-      <Link href="/login">Login</Link>
     </div>
   );
 }
